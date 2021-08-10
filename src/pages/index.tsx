2b2b4c34f72def82cd88600c2b7e580e2c89f38f
YonJun <jhon_08_2007@hotmail.com>
2021-08-10 18:21:01 -0500

    add twin config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import tw, { styled } from 'twin.macro';
+
+const TestTwin = styled.div`
+  ${tw`bg-gray-700 text-gray-100 text-center py-2 text-xl font-bold`}
+`;
+
 const Home = () => {
   return (
-    <div className="bg-gray-700 text-gray-100 text-center py-2 text-xl font-bold">
-      Say hi
-    </div>
+    <>
+      <div className="bg-gray-700 text-gray-100 text-center py-2 text-xl font-bold">
+        Say hi
+      </div>
+      <TestTwin>Say hi</TestTwin>
+    </>
   );
 };
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+  mode: 'jit',
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
