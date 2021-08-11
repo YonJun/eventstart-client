3232ec8a1dc31d5ff25c2ff17ee61afb0d01a356
YonJun <jhon_08_2007@hotmail.com>
2021-08-10 19:18:48 -0500

    testing ESLint, Prettier and Typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
+import { FC } from 'react';
 import tw, { styled } from 'twin.macro';
 
 const TestTwin = styled.div`
   ${tw`bg-gray-700 text-gray-100 text-center py-2 text-xl font-bold`}
 `;
+interface ChildProps {
+  name: string;
+}
+const Child: FC<ChildProps> = ({ name }) => {
+  return <div>{name}</div>;
+};
+interface Child2Props {
+  name: string;
+  prop1: string;
+  prop2: string;
+  prop3: string;
+  prop4: string;
+  prop5: string;
+}
+const Child2: FC<Child2Props> = ({ name }) => {
+  return <div>{name}</div>;
+};
 
 const Home = () => {
   return (
@@ -11,6 +29,16 @@ const Home = () => {
         Say hi
       </div>
       <TestTwin>Say hi</TestTwin>
+      <Child />
+
+      <Child2
+        name="Lorem"
+        prop1="Lorem"
+        prop2="Lorem"
+        prop3="Lorem"
+        prop4="Lorem"
+        prop5="Lorem"
+      />
     </>
   );
 };
